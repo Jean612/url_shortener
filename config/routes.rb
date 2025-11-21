6584b96eefc0849c2f86ba8ae550f4d1014bb6ac
Jean612 <diegojeanpier.dc@gmail.com>
2025-11-20 20:47:09 -0500

    feat: Implement a web UI for URL shortening with a home page, form, and corresponding request specs.

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
+  post "/shorten", to: "home#create", as: :shorten
 end
